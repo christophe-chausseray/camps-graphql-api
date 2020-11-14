6cff5c2640c1996158a10ebd82c8ce394d2784c0
christophe-chausseray <chausseray.christophe@gmail.com>
2020-11-14 11:17:21 +1000

    Init graphql

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,30 @@
 import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { GraphQLSchema, GraphQLObjectType, GraphQLString } from 'graphql';
 
 const app = express();
+
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'RootQueryType',
+        fields: {
+            hello: {
+                type: GraphQLString,
+                resolve() {
+                    return 'world';
+                }
+            }
+        }
+    })
+});
+
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema,
+        graphiql: 'production' !== process.env.NODE_ENV
+    })
+);
 
 app.listen('3000', () => {
     console.log('Server started at http://localhost:3000');
