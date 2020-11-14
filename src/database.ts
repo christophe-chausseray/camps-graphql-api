6112dc6d6e5152abf496d8fe022986bcbbda5abd
christophe-chausseray <chausseray.christophe@gmail.com>
2020-11-14 13:02:16 +1000

    Add dotenv dependency and configure env variable for the postgres db connection

--- src/database.ts
+++ src/database.ts
@@ -1,13 +1,17 @@
 import knex from 'knex';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const database = knex({
     client: 'pg',
+    debug: 'production' !== process.env.NODE_ENV,
     connection: {
-        host: '127.0.0.1',
-        port: 5432,
-        user: 'postgres',
-        password: 'postgres',
-        database: 'camps'
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
     }
 });
 
