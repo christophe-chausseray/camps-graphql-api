6304546be24a6f3de9b3b519c7c22b1d24be3075
christophe-chausseray <chausseray.christophe@gmail.com>
2020-11-20 18:01:17 +1000

    Add server.ts in the ignored path for the code coverage

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,8 @@ module.exports = {
   testEnvironment: 'node',
   collectCoverage: true,
   coverageReporters: ['text-summary', 'html'],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/src/infrastructure/server/express/server.ts',
+  ],
   testPathIgnorePatterns: ['<rootDir>/src/__tests__/helper/server.ts'],
 };
