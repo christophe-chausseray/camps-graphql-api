648547615b3f4b0315b8e5250c878a0a783fc031
christophe-chausseray <chausseray.christophe@gmail.com>
2020-12-18 16:53:11 +0100

    Add apollo-express-server instead of express-graphql

--- src/infrastructure/api/graphql/index.ts
+++
@@ -1,3 +0,0 @@
-import graphqlHTTPOptions from './graphqlOptions';
-
-export { graphqlHTTPOptions };
--- src/infrastructure/server/express/routing.ts
+++
@@ -1,9 +0,0 @@
-import { Express } from 'express';
-import { graphqlHTTP } from 'express-graphql';
-import { graphqlHTTPOptions } from './../../api/graphql';
-
-function initRouting(app: Express): void {
-  app.use('/graphql', graphqlHTTP(graphqlHTTPOptions));
-}
-
-export { initRouting };
