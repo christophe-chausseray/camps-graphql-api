6ee766ba7280d3953e6059ef252c136133bf75a1
christophe-chausseray <chausseray.christophe@gmail.com>
2021-01-29 17:30:05 +0100

    Add missing unit test for creating detailed camping item read model

--- src/__tests__/unit/domain/camping/model/read/campingItem.test.ts
+++ src/__tests__/unit/domain/camping/model/read/campingItem.test.ts
@@ -1,4 +1,7 @@
-import { createCampingItem } from './../../../../../../domain/camping/model/read';
+import {
+  createCampingItem,
+  createDetailedCampingItem,
+} from './../../../../../../domain/camping/model/read';
 
 test('It create camping item from values', () => {
   const campingItem = createCampingItem(
@@ -27,5 +30,85 @@ test('It cannot create a camping item with id nullable', () => {
 test('It cannot create a camping item with name nullable', () => {
   expect(() => {
     createCampingItem('test-id-1', null, 48.630059, 1.835694);
+  }).toThrow('A name cannot be null when creating a camping item');
+});
+
+test('it can create a detailed camping item', () => {
+  const campingItem = createDetailedCampingItem(
+    'test-id-1',
+    'Camping test',
+    'Camping test description',
+    '/path/to/camping.jpg',
+    '10 rue du test',
+    75000,
+    'Paris',
+    20,
+    3,
+    '135465748',
+    'contact@camping-test.com',
+    'http://www.camping-test.com',
+    48.630059,
+    1.835694
+  );
+
+  expect(campingItem).toEqual({
+    id: 'test-id-1',
+    name: 'Camping test',
+    description: 'Camping test description',
+    image: '/path/to/camping.jpg',
+    address: '10 rue du test',
+    zipcode: 75000,
+    city: 'Paris',
+    nb_spots: 20,
+    nb_stars: 3,
+    phone_number: '135465748',
+    email: 'contact@camping-test.com',
+    website: 'http://www.camping-test.com',
+    location: {
+      longitude: 48.630059,
+      latitude: 1.835694,
+    },
+  });
+});
+
+test('It cannot create a detailed camping item with id nullable', () => {
+  expect(() => {
+    createDetailedCampingItem(
+      null,
+      'Camping test',
+      'Camping test description',
+      '/path/to/camping.jpg',
+      '10 rue du test',
+      75000,
+      'Paris',
+      20,
+      3,
+      '135465748',
+      'contact@camping-test.com',
+      'http://www.camping-test.com',
+      48.630059,
+      1.835694
+    );
+  }).toThrow('An id cannot be null when creating a camping item');
+});
+
+test('It cannot create a camping item with name nullable', () => {
+  expect(() => {
+    createDetailedCampingItem(
+      'test-id-1',
+      null,
+      'Camping test description',
+      '/path/to/camping.jpg',
+      '10 rue du test',
+      75000,
+      'Paris',
+      20,
+      3,
+      '135465748',
+      'contact@camping-test.com',
+      'http://www.camping-test.com',
+      48.630059,
+      1.835694
+    );
   }).toThrow('A name cannot be null when creating a camping item');
 });
